96e32f673719a9053c0a63d16b6cb9c334762631
EhteshamMurtaza <120239986+EhteshamMurtaza@users.noreply.github.com>
2023-02-15 12:15:22 +0500

    Add files via upload

---
+++ app.js
@@ -0,0 +1,466 @@
+// Learning new JS functions
+
+// alert("Welcome Haider!");
+
+// console.log("We are learning JS!");
+
+
+
+
+
+
+// alert(4 + 5);
+// console.log(4 + 5);
+// console.log("5" + "2");
+// console.log('Haider' + " " + "Ali" + " " + 'Shah');
+
+
+
+
+
+
+
+// var student= "Imran";
+// var fee = 400;
+
+// console.log(student);
+
+// console.log(fee);
+
+
+
+// var class1= "Ehtesham"
+// var fee= "3000"
+
+// console.log(class1);
+// console.log(fee);
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// var firstName = "Haider";
+// var lastName = 'Ali';
+
+// console.log("Welcome " + firstName + " " + lastName);
+
+
+
+
+
+
+
+
+
+
+
+// var bill = 800;
+// var tax = 18;
+// var totalTax = bill / 100 * tax;
+// var totalBill = bill + totalTax;
+// console.log(totalBill);
+
+
+
+
+
+
+
+// var salary= 18000;
+// var workingDays= 26;
+// var totalSalary= salary / 31 * workingDays ;
+// console.log(totalSalary);
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// var question = "what is your fathers name?"
+
+// prompt(question);
+
+
+
+
+// var Q1= "Whats your name?";
+// var Q2= "Where are your working now>";
+// var Q3= "Whats your monthly income?";
+
+
+
+// prompt(Q1);
+// prompt(Q2);
+// prompt(Q3);
+
+
+
+
+
+
+
+
+// var userName = prompt('What is your name?');
+
+// console.log(userName);
+
+
+
+
+// var userName = prompt("Username");
+// var userPassword = prompt("Password?");
+
+// console.log(userName);
+// console.log(userPassword);
+
+
+
+// var name = prompt("Username");
+// var password = prompt("Password?");
+
+// alert(name);
+// alert(password);
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// var defName = "Saylani Welfare Internation Trust";
+// var question = 'Where do you study?'
+
+// var instName = prompt(question, defName);
+
+// console.log(instName);
+
+
+
+
+
+// var defPlace = "The Educators Malir Campus";
+// var question = "Where do you work?";
+
+// var placeName = prompt (question, defPlace);
+
+// console.log(placeName);
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// alert("Haider");
+// console.log("Akbar");
+// prompt("What is your name?");
+
+
+
+// alert("Haider");
+// var userName = prompt("What is your name?");
+// console.log(userName);
+
+
+
+
+
+
+
+
+
+// console.log(prompt("what is your name?"));
+
+
+// console.log(prompt("Whats your income?"));
+
+
+
+
+
+
+
+// console.log("1" + "2");
+
+
+
+
+
+// console.log("haider" + " " +  "ali");
+
+
+
+
+
+
+
+
+
+// console.log("2" * "3");
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// var schoolFee = 500;
+// var vadTaxRate = 10;
+// var vadTax = schoolFee / 100 * vadTaxRate;
+// var totalFee = schoolFee + vadTax;              
+
+// console.log(totalFee);
+
+
+
+// schoolFee = 450;
+// vadTaxRate = 50;
+// vadTax = schoolFee / 100 * vadTaxRate;
+// totalFee = schoolFee + vadTax;
+// console.log(totalFee);
+
+
+
+
+
+
+
+
+
+// var num1 = 57;
+// var num2 = 5;
+// var remainder = num1 % num2;
+// console.log(remainder)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// var favNumber = +prompt('what is your favorite number?');
+// var anotherFavNumber = +prompt('what is your another favorite number?');
+
+// console.log(favNumber + anotherFavNumber);
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// var userName;
+// userName = "haider";
+
+// var userName = "haider";
+
+
+
+
+
+
+
+
+
+
+
+
+// var userName, fees, tax;
+
+// userName = "haider";
+// fees = 500;
+// tax = 10;
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// alert();
+// console.log();
+// prompt();
+// document.write();
+
+// var userName = prompt('what is your name?');
+// document.write("<h1>Welcome " + userName + "</h1>");
+
+
+
+
+// var userName = prompt("What is your name?");
+// document.write("<h1>Welcome" + " " + userName + "</h1>");
+
+
+
+
+
+
+
+
+
+
